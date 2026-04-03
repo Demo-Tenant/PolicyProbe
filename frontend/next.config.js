98b234af608bdc2cc91bae9f19adc8f4151d4679
Demo-Tenant <ana27.testuser@gmail.com>
2026-04-02 18:01:19 -0700

    fix(frontend/next.config.js): remediate Fix code vulnerabilities [unifai-repo-scan]

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -21,16 +21,12 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
             key: 'X-Frame-Options',
             value: 'DENY',
           },
           {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
           },
           {
             key: 'Referrer-Policy',
@@ -39,6 +35,14 @@ const nextConfig = {
           {
             key: 'Permissions-Policy',
             value: 'camera=(), microphone=(), geolocation=()',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
           },
           {
             key: 'Content-Security-Policy',
